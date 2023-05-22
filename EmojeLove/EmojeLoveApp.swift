2827f42412501918a59ce3889dc403464209e482
Fred Denis <fredjamo68@gmail.com>
2023-05-22 19:21:41 +0300

    Initial Commit

---
+++ EmojeLove/EmojeLoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojeLoveApp.swift
+//  EmojeLove
+//
+//  Created by Jamoni on 22.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct EmojeLoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
